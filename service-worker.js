eea25748d599225626d57b8d4f19bd210bdbebc3
Hanguyen70 <hathuynguyen94@gmail.com>
2026-05-17 16:42:16 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,7 @@
    Cache version bumps automatically when SW_VERSION changes below.
 */
 
-const SW_VERSION = 'v1.35.1';
+const SW_VERSION = 'v1.36.0';
 const CACHE_NAME = `seahorse-${SW_VERSION}`;
 
 // Pre-cache critical files on install
