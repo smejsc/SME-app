54430922d295031633ecca859c989dd6cc7ef449
Hanguyen70 <hathuynguyen94@gmail.com>
2026-06-24 13:18:01 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -6,7 +6,7 @@
    để force trình duyệt invalidate cache cũ.
 */
 
-const SW_VERSION = 'v3.06.67';
+const SW_VERSION = 'v3.06.68';
 const CACHE_NAME = `seahorse-${SW_VERSION}`;
 
 // Pre-cache critical files on install
