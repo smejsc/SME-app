e8bec1671d8cfd6c6b38a161ff30d3f79e98bff3
Hanguyen70 <hathuynguyen94@gmail.com>
2026-07-02 14:22:25 +0700

    Add files via upload

--- Test/service-worker.js
+++ Test/service-worker.js
@@ -6,7 +6,7 @@
    để force trình duyệt invalidate cache cũ.
 */
 
-const SW_VERSION = 'v3.09.87-p2b-writecut-rev2';
+const SW_VERSION = 'v3.09.87-feecol2';
 const CACHE_NAME = `seahorse-${SW_VERSION}`;
 
 // Pre-cache critical files on install
