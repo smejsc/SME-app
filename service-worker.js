2a707fce35193b95510d02b278bad5844be999ac
Hanguyen70 <hathuynguyen94@gmail.com>
2026-09-22 20:48:31 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -17,7 +17,7 @@
    ⚠ QUAN TRỌNG: tăng SW_VERSION mỗi khi release bản mới của index.html để buộc trình duyệt bỏ cache cũ.
 */
 
-const SW_VERSION = 'v3.65.49';
+const SW_VERSION = 'v3.65.54';
 const CACHE_NAME = `seahorse-${SW_VERSION}`;
 
 // Pre-cache critical files on install
